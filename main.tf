9c09cfdb01e859bcc52f95bc16379abc6cdc7ab8
Hajar <hajar.houari@edu.devinci.fr>
2026-02-09 14:36:20 +0100

    Ressources Docker fonctionnelles (network, volume, container)

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
 provider "docker" {}
+
+resource "docker_network" "td_net" {
+  name = var.network_name
+}
+
+resource "docker_volume" "td_vol" {
+  count = var.use_volume ? 1 : 0
+  name  = var.volume_name
+}
+
+resource "docker_image" "app" {
+  name = var.image_name
+}
+
+resource "docker_container" "app" {
+  name  = var.container_name
+  image = docker_image.app.image_id
+
+  env = [for k, v in var.environment : "${k}=${v}"]
+
+  ports {
+    internal = var.container_port
+    external = var.host_port
+  }
+
+  networks_advanced {
+    name = docker_network.td_net.name
+  }
+
+  dynamic "volumes" {
+    for_each = var.use_volume ? [1] : []
+    content {
+      volume_name    = docker_volume.td_vol[0].name
+      container_path = "/usr/share/nginx/html"
+    }
+  }
+}
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,10 +1,10 @@
-container_name  = "nginx-td"
-image_name      = "nginx:latest"
-host_port       = 8080
-container_port  = 80
-network_name    = "net-td"
-use_volume      = true
-volume_name     = "vol-td"
+container_name = "nginx-td"
+image_name     = "nginx:latest"
+host_port      = 8080
+container_port = 80
+network_name   = "net-td"
+use_volume     = true
+volume_name    = "vol-td"
 
 environment = {
   NGINX_HOST = "localhost"
